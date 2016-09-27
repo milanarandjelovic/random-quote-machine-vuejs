80e71f938bf43823c0868c7857ca39ec647dcba8
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-09-27 20:52:33 +0200

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,18 @@ const elixir = require('laravel-elixir');
 // require('laravel-elixir-vue');
 require('laravel-elixir-vueify');
 require('laravel-elixir-browserify-official');
+require('laravel-elixir-eslint');
 
 elixir(mix => {
   mix.sass('app.scss')
-    .browserify('app.js');
+    .copy('node_modules/font-awesome/fonts/', 'public/fonts')
+    .browserify('app.js')
+    /**
+     * The prefix ! is for excluding files.
+     * Excluding file  'Laravel' is not defined  no-undef and  'Vue' is not defined  no-undef
+     */
+    .eslint([
+      'resources/assets/js/**/*.js',
+      'resources/assets/js/**/*.vue'
+    ]);
 });
